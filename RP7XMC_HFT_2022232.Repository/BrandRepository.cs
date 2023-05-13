add93a4950b81a8cee627277ae1291a4cbbe701a
gyurcsandavid@stud.uni-obuda.hu <gyurcsandavid@stud.uni-obuda.hu>
2023-05-13 15:56:54 +0200

    BrandRepository CRUD

---
+++ RP7XMC_HFT_2022232.Repository/BrandRepository.cs
@@ -0,0 +1,31 @@
+﻿using RP7XMC_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RP7XMC_HFT_2022232.Repository
+{
+    public class BrandRepository : Repository<Brand>, IRepository<Brand>
+    {
+        public BrandRepository(CarDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Brand Read(int id)
+        {
+            return ctx.Brands.FirstOrDefault(t => t.BrandId == id);
+        }
+
+        public override void Update(Brand item)
+        {
+            var old = Read(item.BrandId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
