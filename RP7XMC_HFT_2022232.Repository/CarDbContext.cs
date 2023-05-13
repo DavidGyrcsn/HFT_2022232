e8616fdb1e4b2dabe16a1b8cab7e1c2be4a1267d
gyurcsandavid@stud.uni-obuda.hu <gyurcsandavid@stud.uni-obuda.hu>
2023-05-13 14:52:16 +0200

    InMemoryDatabase

--- RP7XMC_HFT_2022232.Repository/CarDbContext.cs
+++ RP7XMC_HFT_2022232.Repository/CarDbContext.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using RP7XMC_HFT_2022232.Models;
 
 namespace RP7XMC_HFT_2022232.Repository
 {
     public class CarDbContext : DbContext
     {
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Service> Services { get; set; }    
+        public CarDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            optionsBuilder.UseLazyLoadingProxies().UseInMemoryDatabase("Service");
             base.OnConfiguring(optionsBuilder);
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
