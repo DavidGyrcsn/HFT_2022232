2cb93323728df4795c2a65aae2630d1ff3e2936f
gyurcsandavid@stud.uni-obuda.hu <gyurcsandavid@stud.uni-obuda.hu>
2023-05-13 15:45:24 +0200

    "Service" Database

--- RP7XMC_HFT_2022232.Repository/CarDbContext.cs
+++ RP7XMC_HFT_2022232.Repository/CarDbContext.cs
@@ -10,9 +10,9 @@ namespace RP7XMC_HFT_2022232.Repository
 {
     public class CarDbContext : DbContext
     {
-        public DbSet<Car> Cars { get; set; }
+        //public DbSet<Car> Cars { get; set; }
         public DbSet<Brand> Brands { get; set; }
-        public DbSet<Service> Services { get; set; }    
+        //public DbSet<Service> Services { get; set; }    
         public CarDbContext()
         {
             this.Database.EnsureCreated();
@@ -25,6 +25,26 @@ namespace RP7XMC_HFT_2022232.Repository
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Brand>()
+                .HasOne(t => t.Car)
+                .WithMany(t => t.Brands)
+                .HasForeignKey(t => t.CarId);
+
+            modelBuilder.Entity<Brand>()
+                .HasOne(t => t.Service)
+                .WithMany(t => t.Brands)
+                .HasForeignKey(t => t.ServiceId);
+
+            modelBuilder.Entity<Brand>().HasData(
+                new Brand {BrandId = 1, BrandName = "BMW",CarId =1 ,ServiceId =1}
+                );
+            modelBuilder.Entity<Car>().HasData(
+                new Car { CarId = 1, CarName = "E60"}
+                );
+            modelBuilder.Entity<Service>().HasData(
+                new Service { ServiceId = 1, ServiceName = "BMW_Budeapest" }
+                );
+
             base.OnModelCreating(modelBuilder);
         }
     }
