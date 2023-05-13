ea3b2f4482bae58b9fe3a4bad80ab59b4cdf0b12
gyurcsandavid@stud.uni-obuda.hu <gyurcsandavid@stud.uni-obuda.hu>
2023-05-13 15:12:07 +0200

    Repository made

--- RP7XMC_HFT_2022232.Repository/Repository.cs
+++ RP7XMC_HFT_2022232.Repository/Repository.cs
@@ -1,8 +1,34 @@
 ﻿using System;
+using System.Linq;
 
 namespace RP7XMC_HFT_2022232.Repository
 {
-    public class Repository
+    public abstract class Repository<T> : IRepository<T> where T : class
     {
+        protected CarDbContext ctx;
+        public Repository(CarDbContext ctx) 
+        {
+            this.ctx = ctx;
+        }
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            ctx.Set<T>().Remove(Read(id));
+            ctx.SaveChanges();
+        }
+
+        public abstract T Read(int id);
+
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public abstract void Update(T item);
     }
 }
