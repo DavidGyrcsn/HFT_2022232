c48706c3597a6f5bc974a7a045d5f70e9b2c51fb
gyurcsandavid@stud.uni-obuda.hu <gyurcsandavid@stud.uni-obuda.hu>
2023-05-23 17:41:43 +0200

    ICarLogic CRUD

---
+++ RP7XMC_HFT_2022232.Logic/ICarLogic.cs
@@ -0,0 +1,18 @@
+﻿using RP7XMC_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RP7XMC_HFT_2022232.Logic
+{
+    public interface ICarLogic
+    {
+        IQueryable<Car> ReadAll();
+        Car Read(int id);
+        void Create(Car item);
+        void Update(Car item);
+        void Delete(int id);
+    }
+}
