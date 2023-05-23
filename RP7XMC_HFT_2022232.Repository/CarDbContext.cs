20799afb664614263eaf11a9d70a23fd5baf0ace
gyurcsandavid@stud.uni-obuda.hu <gyurcsandavid@stud.uni-obuda.hu>
2023-05-23 19:57:57 +0200

    HasData kieg

--- RP7XMC_HFT_2022232.Repository/CarDbContext.cs
+++ RP7XMC_HFT_2022232.Repository/CarDbContext.cs
@@ -36,14 +36,26 @@ namespace RP7XMC_HFT_2022232.Repository
                 .HasForeignKey(t => t.ServiceId);
 
             modelBuilder.Entity<Brand>().HasData(
-                new Brand {BrandId = 1, BrandName = "BMW",CarId =1 ,ServiceId =1}
+                new Brand {BrandId = 1, BrandName = "BMW",CarId =1 ,ServiceId =1},
+                new Brand { BrandId = 2, BrandName = "MercededsBenz", CarId = 2, ServiceId = 4 },
+                new Brand { BrandId = 3, BrandName = "Audi", CarId = 3, ServiceId = 3 },
+                new Brand { BrandId = 4, BrandName = "Opel", CarId = 5, ServiceId = 2 },
+                new Brand { BrandId = 1, BrandName = "BMW", CarId = 4, ServiceId = 1 }
                 );
             modelBuilder.Entity<Car>().HasData(
-                new Car { CarId = 1, CarName = "E60"}
+                new Car { CarId = 1, CarName = "E60"},
+                new Car { CarId = 2, CarName = "S class" },
+                new Car { CarId = 3, CarName = "A8" },
+                new Car { CarId = 4, CarName = "E39" },
+                new Car { CarId = 5, CarName = "Omega" }
                 );
             modelBuilder.Entity<Service>().HasData(
-                new Service { ServiceId = 1, ServiceName = "BMW_Budeapest" }
-                );
+                new Service { ServiceId = 1, ServiceName = "BMW_Budeapest" },
+                new Service { ServiceId = 2, ServiceName = "opel_szervíz" },
+                new Service { ServiceId = 3, ServiceName = "Lakatos.kft" },
+                new Service { ServiceId = 4, ServiceName = "Benz_márkaszervíz" },
+                new Service { ServiceId = 5, ServiceName = "Renault-Magyarország" }
+                ) ;
 
             base.OnModelCreating(modelBuilder);
         }
