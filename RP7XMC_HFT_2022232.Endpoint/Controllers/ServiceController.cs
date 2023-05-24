707182e34ee7e219cc72c59e837848319e65970a
gyurcsandavid@stud.uni-obuda.hu <gyurcsandavid@stud.uni-obuda.hu>
2023-05-25 00:05:34 +0200

    ServiceController

---
+++ RP7XMC_HFT_2022232.Endpoint/Controllers/ServiceController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RP7XMC_HFT_2022232.Logic;
+using RP7XMC_HFT_2022232.Models;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RP7XMC_HFT_2022232.Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        IServiceLogic logic;
+
+        public ServiceController(IServiceLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        // GET: api/<BrandController>
+        [HttpGet]
+        public IEnumerable<Service> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        // GET api/<BrandController>/5
+        [HttpGet("{id}")]
+        public Service Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        // POST api/<BrandController>
+        [HttpPost]
+        public void Create([FromBody] Service value)
+        {
+            this.logic.Create(value);
+        }
+
+        // PUT api/<BrandController>/5
+        [HttpPut]
+        public void Update(int id, [FromBody] Service value)
+        {
+            this.logic.Update(value);
+        }
+
+        // DELETE api/<BrandController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
