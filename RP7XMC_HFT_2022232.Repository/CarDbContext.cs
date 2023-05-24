af6b727a46e57387a7a1eb0acc64c379c7ca6b9d
gyurcsandavid@stud.uni-obuda.hu <gyurcsandavid@stud.uni-obuda.hu>
2023-05-25 00:25:25 +0200

    Tömbösítve

--- RP7XMC_HFT_2022232.Repository/CarDbContext.cs
+++ RP7XMC_HFT_2022232.Repository/CarDbContext.cs
@@ -35,27 +35,28 @@ namespace RP7XMC_HFT_2022232.Repository
                 .WithMany(t => t.Brands)
                 .HasForeignKey(t => t.ServiceId);
 
-            modelBuilder.Entity<Brand>().HasData(
-                new Brand {BrandId = 1, BrandName = "BMW",CarId =1 ,ServiceId =1,MaintenanceCost = 200000},
-                new Brand { BrandId = 2, BrandName = "MercededsBenz", CarId = 2, ServiceId = 4, MaintenanceCost = 400000 },
-                new Brand { BrandId = 3, BrandName = "Audi", CarId = 3, ServiceId = 3, MaintenanceCost = 300000 },
-                new Brand { BrandId = 4, BrandName = "Opel", CarId = 5, ServiceId = 2 , MaintenanceCost = 50000 },
-                new Brand { BrandId = 1, BrandName = "BMW", CarId = 4, ServiceId = 1 , MaintenanceCost = 300000 }
-                );
-            modelBuilder.Entity<Car>().HasData(
-                new Car { CarId = 1, CarName = "E60"},
-                new Car { CarId = 2, CarName = "S class" },
-                new Car { CarId = 3, CarName = "A8" },
-                new Car { CarId = 4, CarName = "E39" },
-                new Car { CarId = 5, CarName = "Omega" }
-                );
-            modelBuilder.Entity<Service>().HasData(
-                new Service { ServiceId = 1, ServiceName = "BMW_Budeapest" },
-                new Service { ServiceId = 2, ServiceName = "opel_szervíz" },
-                new Service { ServiceId = 3, ServiceName = "Lakatos.kft" },
-                new Service { ServiceId = 4, ServiceName = "Benz_márkaszervíz" },
-                new Service { ServiceId = 5, ServiceName = "Renault-Magyarország" }
-                ) ;
+            modelBuilder.Entity<Brand>().HasData(new Brand[]
+                {
+                new Brand() {BrandId = 1, BrandName = "BMW",CarId =1 ,ServiceId =1,MaintenanceCost = 200000},
+                new Brand() { BrandId = 2, BrandName = "MercededsBenz", CarId = 2, ServiceId = 4, MaintenanceCost = 400000 },
+                new Brand() { BrandId = 3, BrandName = "Audi", CarId = 3, ServiceId = 3, MaintenanceCost = 300000 },
+                new Brand() { BrandId = 4, BrandName = "Opel", CarId = 5, ServiceId = 2 , MaintenanceCost = 50000 },
+                new Brand() { BrandId = 1, BrandName = "BMW", CarId = 4, ServiceId = 1 , MaintenanceCost = 300000 }
+                });
+            modelBuilder.Entity<Car>().HasData(new Car[] {
+                new Car() { CarId = 1, CarName = "E60"},
+                new Car() { CarId = 2, CarName = "S class" },
+                new Car() { CarId = 3, CarName = "A8" },
+                new Car() { CarId = 4, CarName = "E39" },
+                new Car (){ CarId = 5, CarName = "Omega" }
+                });
+            modelBuilder.Entity<Service>().HasData(new Service[] {
+                new Service() { ServiceId = 1, ServiceName = "BMW_Budeapest" },
+                new Service() { ServiceId = 2, ServiceName = "opel_szervíz" },
+                new Service() { ServiceId = 3, ServiceName = "Lakatos.kft" },
+                new Service() { ServiceId = 4, ServiceName = "Benz_márkaszervíz" },
+                new Service() { ServiceId = 5, ServiceName = "Renault-Magyarország" }
+                }) ;
 
             base.OnModelCreating(modelBuilder);
         }
