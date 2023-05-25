e0707e5d7697cbc15410656a30f0af96f29e4c1b
gyurcsandavid@stud.uni-obuda.hu <gyurcsandavid@stud.uni-obuda.hu>
2023-05-25 15:53:35 +0200

    RestServices

--- RP7XMC_HFT_2022232.Client/Program.cs
+++ RP7XMC_HFT_2022232.Client/Program.cs
@@ -4,9 +4,10 @@ namespace RP7XMC_HFT_2022232.Client
 {
     internal class Program
     {
+        static RestService brandrest;
         static void Main(string[] args)
         {
-           
+            brandrest = new RestService("http://localhost:2810", "brand");
         }
     }
 }
