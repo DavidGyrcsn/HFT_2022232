9ab816ddb2a7782c1ac20e979ef8da5d93d34695
gyurcsandavid@stud.uni-obuda.hu <gyurcsandavid@stud.uni-obuda.hu>
2023-06-01 18:11:02 +0200

    Controller

---
+++ RP7XMC_HFT_2022232.Endpoint/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RP7XMC_HFT_2022232.Logic;
+using RP7XMC_HFT_2022232.Models;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RP7XMC_HFT_2022232.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        IBrandLogic brandLogic;
+        ICarLogic carLogic;
+        IServiceLogic serviceLogic;
+
+        public ValuesController(IBrandLogic brandLogic, ICarLogic carLogic, IServiceLogic serviceLogic)
+        {
+            this.brandLogic = brandLogic;
+            this.carLogic = carLogic;
+            this.serviceLogic = serviceLogic;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> HighestCost()
+        {
+            return brandLogic.HighestCost();
+        }
+        [HttpGet]
+        public IEnumerable<string> LowestCost()
+        {
+            return brandLogic.LowestCost();
+        }
+        [HttpGet]
+        public IEnumerable<string> AverageCostForAllBrands()
+        {
+            return brandLogic.AverageCostForAllBrands();
+        }
+        [HttpGet]
+        public IEnumerable<int> MaintenanceCostUnder(int cost)
+        {
+            return brandLogic.MaintenanceCostUnder(cost);
+        }
+        [HttpGet]
+        public int MCUnder(int cost)
+        { 
+            return brandLogic.MCUnder(cost);
+        }
+        [HttpGet]
+        public int MCAbowe(int cost)
+        {
+            return brandLogic.MCAbowe(cost);
+        }
+    }
+}
--- RP7XMC_HFT_2022232.Logic/IBrandLogic.cs
+++ RP7XMC_HFT_2022232.Logic/IBrandLogic.cs
@@ -14,5 +14,11 @@ namespace RP7XMC_HFT_2022232.Logic
         void Create(Brand item);
         void Update(Brand item);
         void Delete(int id);
+        public IEnumerable<string> HighestCost();
+        public IEnumerable<string> LowestCost();
+        public IEnumerable<string> AverageCostForAllBrands();
+        public IEnumerable<int> MaintenanceCostUnder(int cost);
+        public int MCUnder(int cost);
+        public int MCAbowe(int cost);
     }
 }
